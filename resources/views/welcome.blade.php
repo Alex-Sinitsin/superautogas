740183e532755547729991d53e19502fe0728907
Alexey Sinitsin <lehych99@mail.ru>
2022-07-25 16:05:39 +0500

    Laravel - стартовый шаблон для сайта с Tailwind CSS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex">
+        <div class="flex-none w-14 h-14">
+            010
+        </div>
+        <div class="flex-1 w-64">
+            02
+        </div>
+        <div class="flex-1 w-32">
+            03
+        </div>
+    </div>
+</body>
+</html>
